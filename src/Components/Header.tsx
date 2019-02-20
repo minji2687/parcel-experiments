076561f5a6957ac20b8b0380bb8537c8ed6c5f9f
Jo MInji <minji1130@mrblue.com>
2019-02-20 17:04:51 +0900

    first Compnent

---
+++ src/Components/Header.tsx
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+class Header extends Component {
+    render() {
+        return (
+            <>
+             <header>
+                 <img src="" alt="logo"/>
+                 <span>App Management</span>
+                 <button>logout</button>
+             </header>
+            </>
+        );
+    }
+}
+
+export default Header;
